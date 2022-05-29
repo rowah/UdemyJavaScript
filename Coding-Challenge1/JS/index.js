b832d1ed93a26c8f1bbd4c9040d1852937f3a038
Rowa <jrowah@gmail.com>
2022-05-30 00:59:07 +0300

    coding challenge 2

--- Coding-Challenge1/JS/index.js
+++ Coding-Challenge1/JS/index.js
@@ -23,3 +23,16 @@ let markHigherBMI = markBMI > johnBMI;
 // m tall.
 // § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
 // m tall.
+
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+HINT: Use an if/else statement 😉 */
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`);
+}
