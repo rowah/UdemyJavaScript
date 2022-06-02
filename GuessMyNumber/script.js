885fdc414e3947361eb7c690b2718aecfafa576f
Rowa <jrowah@gmail.com>
2022-06-02 14:18:44 +0300

    Changes inner text of elements

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,1 +1,6 @@
-"use strict";
+'use strict';
+const message = document.querySelector('.message').textContent;
+console.log(message);
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
