1acf16d66719252a4b27979d76026e78712a5415
Rowa <jrowah@gmail.com>
2022-06-02 14:38:20 +0300

    Adds event listener

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -4,3 +4,11 @@ console.log(message);
 document.querySelector('.message').textContent = 'Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
+
+//event listensr
+document.querySelector('.check').addEventListener('click', function () {
+  const inputValue = document.querySelector('.guess').value;
+  document.querySelector('.number').textContent = inputValue;
+  //   console.log(inputValue);
+});
+// document.querySelector('.number').textContent = inputValue;
