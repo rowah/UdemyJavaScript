1b5e991aab460f1eafe9e5aa0710291d9dbdd165
Rowa <jrowah@gmail.com>
2022-06-03 22:15:08 +0300

    Checks if number is correct

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -8,7 +8,8 @@
 //define the secret number to compare to the input number after every event handling
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-//display
+//display secret number under the question mark on the dom
+document.querySelector('.number').textContent = secretNumber;
 //event listensr
 document.querySelector('.check').addEventListener('click', function () {
   //Number converts the stringy input value into an actual number
@@ -19,7 +20,14 @@ document.querySelector('.check').addEventListener('click', function () {
   //implementing simplest game logic if no value input
   if (!inputValue) {
     document.querySelector('.message').textContent = 'No Number!';
-  } else if (inputValue) {
+  } else if (inputValue < secretNumber) {
+    document.querySelector('.message').textContent =
+      'Wrong! The number is too low! Guess Again';
+  } else if (inputValue === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (inputValue > secretNumber) {
+    document.querySelector('.message').textContent =
+      'Wrong! The number is too high! Guess Again';
   }
 });
 // document.querySelector('.number').textContent = inputValue;
