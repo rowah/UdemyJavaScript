7d21705a9b51b3cbe50c494fb32fdcc7694d07a3
Rowa <jrowah@gmail.com>
2022-08-14 12:25:18 +0300

    destructures obj

--- restaurant/js/script.js
+++ restaurant/js/script.js
@@ -1,41 +1,41 @@
-"use strict";
+// "use strict";
 
-const restaurant = {
-  name: "White Elephant",
-  location: "Rsuinga Island, HomaBay, Kenya",
-  categories: ["Vegarian", "Organic", "Italian", "Pizzeria"],
-  starterMenu: ["Focassia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Beef", "Fish", "Pizza"],
-  //method to order food
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-};
+// const restaurant = {
+//   name: "White Elephant",
+//   location: "Rsuinga Island, HomaBay, Kenya",
+//   categories: ["Vegarian", "Organic", "Italian", "Pizzeria"],
+//   starterMenu: ["Focassia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Beef", "Fish", "Pizza"],
+//   //method to order food
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+// };
 
-//array destructuring
-const arr = [1, 2, 3];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// //array destructuring
+// const arr = [1, 2, 3];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-const [x, y, z] = arr;
-console.log(x, y, z);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
 
-//taking the first two restaurant categories
-let [first, second, , forth] = restaurant.categories;
-console.log(first, second, forth); //gives Vegarian, Organic and Pizzeria
-//reassgning
-[first, second] = [second, first];
-console.log(first, second);
-console.log(restaurant.order(0, 2)); //can now be destructured
+// //taking the first two restaurant categories
+// let [first, second, , forth] = restaurant.categories;
+// console.log(first, second, forth); //gives Vegarian, Organic and Pizzeria
+// //reassgning
+// [first, second] = [second, first];
+// console.log(first, second);
+// console.log(restaurant.order(0, 2)); //can now be destructured
 
-//destructruing with nested array
-const nested = [1, 2, 3, [5, 6]];
-// const [i, , , j] = nested;
-// console.log(i, j);
-const [i, , , [j]] = nested;
-console.log(j);
+// //destructruing with nested array
+// const nested = [1, 2, 3, [5, 6]];
+// // const [i, , , j] = nested;
+// // console.log(i, j);
+// const [i, , , [j]] = nested;
+// console.log(j);
 
-const [t, m, k] = [5, 7];
-console.log(t, m, k);
-//k becomes undefined
+// const [t, m, k] = [5, 7];
+// console.log(t, m, k);
+// //k becomes undefined but k can be assigned to one
---
+++ restaurant/js/script2.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const restaurant = {
+  name: "White Elephant",
+  location: "Rsuinga Island, HomaBay, Kenya",
+  categories: ["Vegarian", "Organic", "Italian", "Pizzeria"],
+  starterMenu: ["Focassia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Beef", "Fish", "Pizza"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+  //method to order food
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
+};
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Hunters 14th Street",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+//object destructuring using curly braces
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//destructuring objs
+let a = 111;
+let b = 234;
+const obj = {
+  a: 23,
+  b: 45,
+  c: 12,
+};
+({ a, b } = obj);
+console.log(a, b); //23 45
+
+//nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
