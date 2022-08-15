c398cb0b6f307e95f6babbd54a479197ab5bb40a
Rowa <jrowah@gmail.com>
2022-08-15 22:46:29 +0300

    sets up env

---
+++ challenge#2/js/script.js
@@ -0,0 +1,12 @@
+.
+
+
+
+
+
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
