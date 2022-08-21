22881895dd6a71a02d35b9d22ec1550284d95f44
Rowa <jrowah@gmail.com>
2022-08-21 10:21:33 +0300

    prints bank acc movements: depos and withrawals

--- BankListApp/js/script.js
+++ BankListApp/js/script.js
@@ -72,3 +72,31 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//looping over to print each movement for bank account: pos values are deposits, negative values are withdrwals
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited $${movement}`);
+//   } else {
+//     console.log(`You withdrew $${Math.abs(movement)}`);
+//   }
+// }
+
+//OR FOREACH
+// movements.forEach(function (movement) {
+//   if (movement > 0) {
+//     console.log(`You deposited $${movement}`);
+//   } else {
+//     console.log(`You withdrew $${Math.abs(movement)}`);
+//   }
+// });
+
+//OR FUNCTION CALL
+const moveThrough = function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited $${movement}`);
+  } else {
+    console.log(`You withdrew $${Math.abs(movement)}`);
+  }
+};
+
+movements.forEach(moveThrough);
